bbdff1118b1f8d8d07763a62e466f3dcf28f988e
Olivia Tangredi oliviatangredixo@gmail.com <oliviatangredixo@gmail.com>
2022-11-08 12:28:41 -0500

    index, show, new, create methods fully completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # verb '/path', to: 'controller#action', as: :prefix
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Read all
+  get '/tasks', to: 'tasks#index'
+
+  # Create
+  # 1. Provide the form
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  # 2. Send the form
+  post '/tasks', to: 'tasks#create'
+
+  # Read one
+  get '/tasks/:id', to: 'tasks#show', as: :task
 end
